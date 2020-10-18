8a4be88761f60a2bede3ad8fd94d1e8949dae310
Dominika Fudala <fud.dominika@gmail.com>
2020-10-18 15:47:49 +0200

    Final sort

---
+++ Zad_1/sort.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    if(argc == 1) { // checking if there are any arguments
+        cout<<"No arguments!"<<endl;
+        return 0;
+    }
+
+    vector<int>numbers;
+
+    for(int i = 1; i < argc; i++ ) { 
+        int num = atoi(argv[i]); // string to int conversion
+        numbers.push_back(num); // adding numbers to vector
+    } 
+
+	for (int i = 1; i < numbers.size(); i++) // loop for sorting
+	{
+        int count = 0;
+		int x = numbers[i];
+		int j = i - 1;
+		while (j >= 0 && numbers[j] > x)
+		{
+			numbers[j + 1] = numbers[j];
+			j--;
+			count++;
+		}
+		numbers[j + 1] = x;
+	}
+
+     for(int i = 0; i < numbers.size(); i++) { // printing output
+        cout<<numbers[i]<<" ";
+    }
+
+    cout<<endl;
+
+    return 0;  
+
+
+}
